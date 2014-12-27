2651c677a2c01dc5625188e4fc9ff86a9a87b257
Bence Kiglics <bence@kiglics.hu>
2014-12-27 20:26:54 +0100

    initial commit

---
+++ src/smoke.rb
@@ -0,0 +1,32 @@
+require 'sinatra/base'
+require 'sinatra/assetpack'
+
+class SmokeApp < Sinatra::Base
+  register Sinatra::AssetPack
+
+  configure do
+    set :port, ENV['PORT'] || 8080
+  end
+
+  assets {
+    serve '/js', from: 'js'
+    serve '/css', from: 'css'
+
+    js :app, [
+      '/js/**/*.js'
+    ]
+
+    css :app, [
+      '/css/*.css'
+    ]
+
+    js_compression :jsmin
+    css_compression :simple
+  }
+
+  get "/" do
+    erb :index
+  end
+
+  run!
+end
